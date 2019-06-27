73853188e7ccc79a419a5c847de624e24eeb4798
romanjke <romanjkez0r@gmail.com>
2019-06-27 08:29:01 +0300

    added book and author repositories

---
+++ app/Repositories/AuthorRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Repository;
+
+class AuthorRepository extends Repository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return mixed
+     */
+    function model()
+    {
+        return 'App\Author';
+    }
+}
---
+++ app/Repositories/RepositoryException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Class RepositoryException
+ * @package App\Repository
+ */
+class RepositoryException extends \Exception {}
